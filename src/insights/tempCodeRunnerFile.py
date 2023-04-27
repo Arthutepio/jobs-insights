0352b4345f40b727ec9bdc9bee71364635899cf3
arthutepio <arthurodriguesr10@gmail.com>
2023-04-27 16:07:37 -0300

    fix: exclude file tempCodeRunnerFile.py

--- src/insights/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def read():
-    with open("arquivo.csv", encoding="utf8") as file:
-        data = csv.reader(file, delimiter=",", quotechar='"')
-        print(data)
-
-    raise NotImplementedError
