5318742e476bb5dbbf71cfcd06ced0c82f309f52
arthutepio <arthurodriguesr10@gmail.com>
2023-04-28 21:07:00 -0300

    test: implements test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    expected_count_word_python = 1639
+    expected_count_word_javascript = 122
+    count_word_python = count_ocurrences('data/jobs.csv', 'Python')
+    count_word_javascript = count_ocurrences('data/jobs.csv', 'JavaScript')
+    assert count_word_python == expected_count_word_python
+    assert count_word_javascript == expected_count_word_javascript
