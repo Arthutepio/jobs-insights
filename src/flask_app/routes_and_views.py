d2ad0a18a30b31f97fc3b892b9529e9fc1891631
Arthutepio <arthurodriguesr10@gmail.com>
2023-05-06 13:04:11 -0300

    fix: fixed route job

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -20,7 +20,7 @@ from src.flask_app.more_insights import (
     slice_jobs,
     get_int_from_args,
     build_jobs_urls,
-    get_job
+    get_job,
 )
 
 bp = Blueprint("client", __name__, template_folder="templates")
@@ -80,9 +80,9 @@ def list_jobs():
     return render_template("list_jobs.jinja2", ctx=ctx)
 
 
-@bp.route("/jobs/<index>")
+@bp.route("/job/<index>")
 def job(index):
-    jobs = read("data/jobs.csv")
+    jobs = read(path="data/jobs.csv")
     job = get_job(jobs, index)
     return render_template("job.jinja2", job=job)
 
