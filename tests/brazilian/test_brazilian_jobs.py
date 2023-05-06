15a57228d677c0ff676416ee5c26c73187ed3b4e
Arthutepio <arthurodriguesr10@gmail.com>
2023-05-06 10:52:23 -0300

    test: test read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    expected_result = {
+        "title": "Maquinista", "salary": "2000", "type": "trainee"
+    }
+    job_list = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert job_list[0] == expected_result
